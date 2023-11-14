79774878d0665a2a34a98fb75eb3f54773e0d25f
MinVitoria <yasminvitoriac18@gmail.com>
2023-11-14 10:05:29 -0300

    Inicio do back

---
+++ back/app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const cors= require("cors")
+
+const mysql= require("mysql2/promise")
+
+const app = express()
+app.use(cors())
+const porta = 3306
+
+app.listen(porta, ()=> {
+    console.log(`Servidor rodeando em http://localhost:${porta}`)
+})
+
+// criar pool de conexão
+const pool = mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'db_node',
+    waitForConnections:true,
+    connectionLimit: 3,
+    queueLimit:0
+})
